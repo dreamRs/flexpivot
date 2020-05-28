592fa2a098d7affefe2733590c9e1f05ded24d12
pvictor <perrier.victor@gmail.com>
2020-05-28 15:16:44 +0200

    app v0

---
+++ dev/app-pivot.R
@@ -0,0 +1,53 @@
+
+
+# Test addin --------------------------------------------------------------
+
+library(shiny)
+library(esquisse)
+library(flextable)
+library(flexpivot)
+
+
+data("diamonds", package = "ggplot2")
+
+ui <- fluidPage(
+  tags$h3("flexpivot"),
+  dragulaInput(
+    inputId = "vars",
+    sourceLabel = "Variables",
+    targetsLabels = c("row", "col", "value"),
+    choices = names(diamonds),
+    replace = TRUE
+  ),
+  # verbatimTextOutput("test")
+  uiOutput(outputId = "table")
+)
+
+server <- function(input, output, session) {
+
+  output$test <- renderPrint({
+    input$vars
+  })
+
+  pivot_r <- reactive({
+    req(input$vars$target$row)
+
+    pivot_table(
+      data = diamonds,
+      rows = input$vars$target$row,
+      cols = input$vars$target$col
+    )
+  })
+
+  output$table <- renderUI({
+    htmltools_value(flextable(pivot_r()), class = "table table-responsive")
+  })
+
+}
+
+runGadget(
+  app = ui,
+  server = server,
+  viewer = dialogViewer(dialogName = "", width = 1000, height = 750)
+)
+
